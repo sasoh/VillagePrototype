79783557ed53e8ecd92a6f0f476381c73f2c7096
Sasho <lxndrppv@gmail.com>
2014-04-06 15:12:11 +0300

    Changed materials to phong; rotating body now a cube

--- logic.js
+++ logic.js
@@ -3,10 +3,52 @@ var scene;
 var renderer;
 var mesh;
 
-
 function degreesToRadians(degrees) {
 
   return degrees * (Math.PI / 180);
+
+}
+
+function initSampleSceneGeometry() {
+
+    // dummy mesh to show something alive on the scene
+
+  var phongMaterial = new THREE.MeshPhongMaterial({
+    overdraw: true,
+    color: 0xCCCCCC
+  })
+
+  var light = new THREE.DirectionalLight(0xF6E86D, 1);
+  light.position.set(1, 3, 2);
+  scene.add(light);
+
+//  var mgeometry = new THREE.IcosahedronGeometry(150, 1);
+  var mgeometry = new THREE.CubeGeometry(100, 100, 100);
+  var mmaterial = new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    wireframe: true,
+    wireframeLinewidth: 2
+  });
+  mesh = new THREE.Mesh(mgeometry, phongMaterial);
+  mesh.position.y = 90;
+  scene.add(mesh);
+
+  // ground plane
+
+  var wgeometry = new THREE.PlaneGeometry(1000, 1000, 25, 25);
+//  var image = THREE.ImageUtils.loadTexture('./Resources/io_background.jpg'); // 2500x1400
+//  var wmaterial = new THREE.MeshBasicMaterial({
+//    map: image
+//  });
+  var wmaterial = new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    wireframe: true,
+    wireframeLinewidth: 1
+  });
+  mesh
+  var wireplane = new THREE.Mesh(wgeometry, phongMaterial);
+  wireplane.rotation.x = degreesToRadians(-90); // initially the plane is in the XY-plane
+  scene.add(wireplane);
 
 }
 
@@ -14,7 +56,7 @@ function init() {
 
   // renderer
 
-  renderer = new THREE.CanvasRenderer();
+  renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
@@ -34,34 +76,7 @@ function init() {
 
   scene = new THREE.Scene();
 
-  // dummy mesh to show something alive on the scene
-
-  var mgeometry = new THREE.IcosahedronGeometry(150, 1);
-  var mmaterial = new THREE.MeshBasicMaterial({
-    color: 0x000000,
-    wireframe: true,
-    wireframeLinewidth: 2
-  });
-  mesh = new THREE.Mesh(mgeometry, mmaterial);
-  mesh.position.y = 90;
-  scene.add(mesh);
-
-  // ground plane
-
-  var wgeometry = new THREE.PlaneGeometry(1000, 1000, 25, 25);
-//  var image = THREE.ImageUtils.loadTexture('./Resources/io_background.jpg'); // 2500x1400
-//  var wmaterial = new THREE.MeshBasicMaterial({
-//    map: image
-//  });
-  var wmaterial = new THREE.MeshBasicMaterial({
-    color: 0x000000,
-    wireframe: true,
-    wireframeLinewidth: 1
-  });
-  mesh
-  var wireplane = new THREE.Mesh(wgeometry, wmaterial);
-  wireplane.rotation.x = degreesToRadians(-90); // initially the plane is in the XY-plane
-  scene.add(wireplane);
+  initSampleSceneGeometry();
 
 }
 
