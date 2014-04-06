e0b24d2b8bbcc27f6fbfa2c4424dd9a3521eec29
Sasho <lxndrppv@gmail.com>
2014-04-06 14:42:34 +0300

    Moved stuff around, added some comments

--- logic.js
+++ logic.js
@@ -3,41 +3,84 @@ var scene;
 var renderer;
 var mesh;
 
+
+function degreesToRadians(degrees) {
+
+  return degrees * (Math.PI / 180);
+
+}
+
 function init() {
 
-  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
-  camera.position.y = 500;
-  camera.position.z = 500;
-  camera.rotation = new THREE.Vector3(-1 , 0, 0);
-
-  scene = new THREE.Scene();
-
-  var mgeometry = new THREE.IcosahedronGeometry(200, 1);
-  var mmaterial = new THREE.MeshBasicMaterial({ color: 0x0A0A0A, wireframe: true, wireframeLinewidth: 2});
-  mesh = new THREE.Mesh(mgeometry, mmaterial);
-  scene.add(mesh);
-
-  var wgeometry = new THREE.PlaneGeometry( 1000, 1000, 25, 25 );
-  var wmaterial = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, wireframeLinewidth: 1 } );
-  var wireplane = new THREE.Mesh(wgeometry, wmaterial);
-  wireplane.scale.set( 1, 1, 1 );
-  wireplane.rotation.x = - Math.PI / 2;
-  scene.add(wireplane);
+  // renderer
 
   renderer = new THREE.CanvasRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
+  // camera
+
+  var fieldOfView = 75;
+  var aspectRatio = window.innerWidth / window.innerHeight;
+  var nearPlane = 1;
+  var farPlane = 1000;
+  camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
+  // raise the camera a bit & rotate toward the ground
+  camera.position.y = 500;
+  camera.position.z = 500;
+  camera.rotation.x = degreesToRadians(-60);
+
+  // scene & geometry
+
+  scene = new THREE.Scene();
+
+  // dummy mesh to show something alive on the scene
+
+  var mgeometry = new THREE.IcosahedronGeometry(150, 1);
+  var mmaterial = new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    wireframe: true,
+    wireframeLinewidth: 2
+  });
+  mesh = new THREE.Mesh(mgeometry, mmaterial);
+  mesh.position.y = 90;
+  scene.add(mesh);
+
+  // ground plane
+
+  var wgeometry = new THREE.PlaneGeometry(1000, 1000, 25, 25);
+//  var image = THREE.ImageUtils.loadTexture('./Resources/io_background.jpg'); // 2500x1400
+//  var wmaterial = new THREE.MeshBasicMaterial({
+//    map: image
+//  });
+  var wmaterial = new THREE.MeshBasicMaterial({
+    color: 0x000000,
+    wireframe: true,
+    wireframeLinewidth: 1
+  });
+  mesh
+  var wireplane = new THREE.Mesh(wgeometry, wmaterial);
+  wireplane.rotation.x = degreesToRadians(-90); // initially the plane is in the XY-plane
+  scene.add(wireplane);
+
 }
 
 function animate() {
 
-  requestAnimationFrame(animate);
-  mesh.rotation.x = Date.now() * 0.00005;
-  mesh.rotation.y = Date.now() * 0.0001;
+  mesh.rotation.x = Date.now() * 0.0001;
+  mesh.rotation.y = Date.now() * 0.0002;
   renderer.render(scene, camera);
+
+  requestAnimationFrame(animate);
 
 }
 
-init();
-animate();
+function run() {
+
+  init();
+
+  animate();
+
+}
+
+run();
