83b37ac20d5dc7623f5207321a9e999c52c7a0cd
Sasho <lxndrppv@gmail.com>
2014-04-21 22:34:57 +0300

    Added basic GUI
    moved stuff around; loading wireframe of a COLLADA model

---
+++ js/logic.js
@@ -0,0 +1,229 @@
+var camera;
+var scene;
+var renderer;
+var mesh;
+var controls;
+var clock;
+var gui;
+var guiControls;
+var controllerActive;
+var currentFov;
+var currentCameraRotation;
+
+function degreesToRadians(degrees) {
+
+	return degrees * (Math.PI / 180);
+
+}
+
+var guiText = function() {
+
+	this.fov = currentFov;
+	this.cameraAngle = currentCameraRotation;
+	this.groundRotation = 0;
+
+	this.explode = function() {
+		// Define render logic ...
+	};
+
+};
+
+function initGui() {
+	
+	gui = new dat.GUI();
+	guiControls = new guiText();
+
+	// fov
+	var minFov = 30;
+	var maxFov = 95;
+	var fovController = gui.add(guiControls, 'fov', minFov, maxFov).listen();
+	fovController.onChange(function(value) {
+	
+		camera.fov = value;
+		camera.updateProjectionMatrix();
+		
+		controllerActive = false;
+		
+	});
+	fovController.onFinishChange(function() {
+	
+		controllerActive = true;
+		
+	});
+	
+	// camera angle
+	var minAngle = 0;
+	var maxAngle = 90;
+	var angleController = gui.add(guiControls, 'cameraAngle', minAngle, maxAngle)
+	angleController.onChange(function(value) {
+	
+		camera.rotation.x = degreesToRadians(-value);
+		camera.updateProjectionMatrix();
+		
+		controllerActive = false;
+		
+	});
+	angleController.onFinishChange(function() {
+	
+		controllerActive = true;
+		
+	});
+	
+	// ground rotation
+	var minGroundAngle = -180;
+	var maxGroundAngle = 180;
+	var groundAngleController = gui.add(guiControls, 'groundRotation', minGroundAngle, maxGroundAngle);
+	groundAngleController.onChange(function(value) {
+	
+		mesh.rotation.z = degreesToRadians(-value);
+		
+		controllerActive = false;
+		
+	});
+	groundAngleController.onFinishChange(function() {
+	
+		controllerActive = true;
+		
+	});
+	
+	// bounds left/right
+	
+	// bounds up/down
+	
+	// materials
+	// solid
+	// wireframe
+	// textured (unavailable yet)
+	
+	// keyboard controls
+	
+}
+
+function initSampleSceneGeometry() {
+
+	// dummy mesh to show something alive on the scene
+	var phongMaterial = new THREE.MeshPhongMaterial({
+		overdraw: true,
+		color: 0xCCCCCC
+	})
+
+	// single directional light
+	var light = new THREE.DirectionalLight(0xF6E86D, 1);
+	light.position.set(1, 3, 2);
+	scene.add(light);
+
+	var mgeometry = new THREE.CubeGeometry(100, 100, 100);
+	var mmaterial = new THREE.MeshBasicMaterial({
+		color: 0x000000,
+		wireframe: true,
+		wireframeLinewidth: 2
+	});
+	mesh = new THREE.Mesh(mgeometry, phongMaterial);
+	mesh.position.y = 90;
+	scene.add(mesh);
+
+	// ground plane
+	var wgeometry = new THREE.PlaneGeometry(1000, 1000, 25, 25);
+	var image = THREE.ImageUtils.loadTexture('./Resources/io_background.jpg'); // 2500x1400
+ 	// var wmaterial = new THREE.MeshBasicMaterial({
+		// map: image
+	// });
+	var wmaterial = new THREE.MeshBasicMaterial({
+		color: 0x000000,
+		wireframe: true,
+		wireframeLinewidth: 1
+	});
+	
+	var wireplane = new THREE.Mesh(wgeometry, phongMaterial);
+	wireplane.rotation.x = degreesToRadians(-90); // initially the plane is in the XY-plane
+	scene.add(wireplane);
+	
+}
+
+function loadCustomModels() {
+	
+	var loader = new THREE.ColladaLoader();
+	loader.load('Resources/models/ground.dae', function(result) {
+		mesh = result.scene;
+		mesh.rotation.x = degreesToRadians(-90);
+		var wireframeMaterial = new THREE.MeshBasicMaterial({
+			wireframe: true,
+			color: 'blue'
+		});
+		
+		 mesh.traverse( function ( child ) {
+			if ( child instanceof THREE.Mesh ) {
+				child.material = wireframeMaterial;
+			}
+		} );
+	
+		scene.add(mesh);
+		console.log(mesh);
+	});
+
+}
+
+function onWindowResize() {
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+}
+
+function init() {
+
+	clock = new THREE.Clock();
+	
+	// renderer
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor(0xcccccc, 1);
+	document.body.appendChild(renderer.domElement);
+	
+	window.addEventListener('resize', onWindowResize, false );
+
+	// camera
+	currentFov = 75;
+	var aspectRatio = window.innerWidth / window.innerHeight;
+	var nearPlane = 10;
+	var farPlane = 500;
+	camera = new THREE.PerspectiveCamera(currentFov, aspectRatio, nearPlane, farPlane);
+	// raise the camera a bit & rotate toward the ground
+	currentCameraRotation = 60;
+	camera.position.y = 50;
+	camera.position.z = 50;
+	camera.rotation.x = degreesToRadians(-currentCameraRotation);
+		
+	// control
+	controls = new rtsCameraControl(camera);
+	controls.moveSpeed = 1;
+	controls.lookSpeed = 0.005;
+	controllerActive = true;
+
+	// scene & geometry
+	scene = new THREE.Scene();
+	// scene.fog = new THREE.FogExp2(0x9DB3B5, 0.0005);
+	
+}
+
+function animate() {
+
+	renderer.render(scene, camera);
+	controls.update(clock.getDelta(), controllerActive);
+	guiControls.fov = camera.fov;
+	requestAnimationFrame(animate);
+
+}
+
+function run() {
+
+	init();
+	initGui();
+	// initSampleSceneGeometry();
+	loadCustomModels();
+	animate();
+
+}
+
+run();
---
+++ js/rtsCameraControl.js
@@ -0,0 +1,185 @@
+function rtsCameraControl(camera, options) {
+	
+	this.camera = camera;
+	options = options || {};
+	this.domElement = options.domElement || document;
+	this.moveSpeed = options.moveSpeed || 1;
+	this.lookSpeed = options.lookSpeed || 0.1;
+	this.domElement.addEventListener('keydown', this.onKeyDown.bind(this), false);
+	this.domElement.addEventListener('keyup', this.onKeyUp.bind(this), false);
+	this.domElement.addEventListener('mousedown', this.onTouchStart.bind(this), false );
+	this.domElement.addEventListener('mousemove', this.onTouchMove.bind(this), false );
+	this.domElement.addEventListener('mouseup', this.onTouchEnd.bind(this), false );
+	this.domElement.addEventListener('mousewheel', this.onMouseWheel.bind(this), false );
+	this.domElement.addEventListener('DOMMouseScroll', this.onMouseWheel.bind(this), false ); // firefox
+	
+	this.dragging = false;
+	this.mouseVector = new THREE.Vector3();
+	
+}
+
+function checkMapBounds() {
+		
+	// camera movement bounds
+	var halfMapSide = 50; // current sample plane size is 1000x1000, this would place the bounds a bit to the inside
+	if (this.camera.position.x < -halfMapSide) {
+		this.camera.position.x = -halfMapSide;
+	}
+	if (this.camera.position.x > halfMapSide) {
+		this.camera.position.x = halfMapSide;
+	}
+	
+	// z offset is the distance from the projection of the camera position to the looking point
+	var zOffset = this.camera.position.y / Math.tan(this.camera.rotation.x);
+	if (this.camera.position.z + zOffset < -halfMapSide) {
+		this.camera.position.z = -halfMapSide - zOffset;
+	}
+	if (this.camera.position.z + zOffset > halfMapSide) {
+		this.camera.position.z = halfMapSide - zOffset;
+	}
+	
+}
+
+rtsCameraControl.prototype = {
+
+	update: function(delta, active) {
+		// forward/backward movement will be parallel to the XZ axis
+		if (active == true) {
+			if (this.moveForward) {
+				this.camera.translateZ(-this.moveSpeed * Math.cos(this.camera.rotation.x));
+				this.camera.translateY(-this.moveSpeed * Math.cos(Math.PI / 2 - this.camera.rotation.x));
+			}
+			if (this.moveBackward) {
+				this.camera.translateZ(this.moveSpeed * Math.cos(this.camera.rotation.x));
+				this.camera.translateY(this.moveSpeed * Math.cos(Math.PI / 2 - this.camera.rotation.x));
+			}
+			if (this.moveLeft) {
+				this.camera.translateX(-this.moveSpeed);
+			}
+			if (this.moveRight) {
+				this.camera.translateX(this.moveSpeed);
+			}
+
+			this.camera.position.add(this.mouseVector);
+			
+			checkMapBounds();
+		}
+	},
+	onKeyDown: function (event) {
+		switch (event.keyCode) {
+			case 38: /*up*/
+			case 87: /*W*/ {
+				this.moveForward = true; 
+				break;
+			}
+			case 37: /*left*/
+			case 65: /*A*/ {
+				this.moveLeft = true;
+				break;
+			}
+			case 40: /*down*/
+			case 83: /*S*/ {
+				this.moveBackward = true; 
+				break;
+			}
+			case 39: /*right*/
+			case 68: /*D*/ {
+				this.moveRight = true; 
+				break;
+			}
+		}
+	},
+	onKeyUp: function (event) {
+		switch(event.keyCode) {
+			case 38: /*up*/
+			case 87: /*W*/ {
+				this.moveForward = false; 
+				break;
+			}
+			case 37: /*left*/
+			case 65: /*A*/ {
+				this.moveLeft = false;
+				break;
+			}
+			case 40: /*down*/
+			case 83: /*S*/ {
+				this.moveBackward = false; 
+				break;
+			}
+			case 39: /*right*/
+			case 68: /*D*/ {
+				this.moveRight = false; 
+				break;
+			}
+		}
+	},
+	onTouchStart: function (event) {
+	
+		// check for object intersection?
+		
+		// if no intersection, start dragging
+		this.dragging = true;
+		this.startPositionX = event.clientX;
+		this.startPositionY = event.clientY;
+		
+	},
+	onTouchMove: function (event) {
+	
+		this.mouseVector.set(0, 0, 0);
+		var currentPositionX = event.clientX;
+		var currentPositionY = event.clientY;
+		
+		if (this.dragging == true) {
+			this.mouseVector.set(this.lookSpeed  * (this.startPositionX - currentPositionX), 0, this.lookSpeed * (this.startPositionY - currentPositionY));
+		}
+		// } else {
+			// autoscroll
+			// var width = this.domElement.body.clientWidth;
+			// var height = this.domElement.body.clientHeight;
+			
+			// var screenBorder = 10; // this should probably be a percent, not a fixed value
+			// if (currentPositionX < screenBorder) {
+				// this.mouseVector.x = -this.moveSpeed;
+			// }
+			// if (currentPositionX > width - screenBorder) {
+				// this.mouseVector.x = this.moveSpeed;
+			// }
+			// if (currentPositionY < screenBorder) {
+				// this.mouseVector.z = -this.moveSpeed;
+			// }
+			// if (currentPositionY > height - screenBorder) {
+				// this.mouseVector.z = this.moveSpeed;
+			// }
+		// }
+
+	},
+	onTouchEnd: function (event) {
+	
+		this.dragging = false;
+		this.mouseVector.set(0, 0, 0);
+		
+	},
+	onMouseWheel: function (event) {
+	
+		var delta = 0;
+
+		if (event.wheelDelta) { // WebKit / Opera / Explorer 9
+			delta = event.wheelDelta / 40;
+		} else if (event.detail) { // Firefox
+			delta = -event.detail / 3;
+		}
+		
+		var currentFov = this.camera.fov;
+		currentFov -= delta;
+		if (currentFov < 30) {
+			currentFov = 30;
+		}
+		if (currentFov > 95) {
+			currentFov = 95;
+		}
+		this.camera.fov = currentFov;
+		this.camera.updateProjectionMatrix();
+		
+	}
+
+};
